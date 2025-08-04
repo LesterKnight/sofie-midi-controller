f7a9bdfbc0fb07972af8f2e84470ccd2b3d775c2
Oliveira Miranda <fishermen18@gmail.com>
2025-08-04 01:11:22 -0300

    Implemented MenuNode structure and initial browse functionality

--- sofie_midi_controller/sofie_midi_controller.ino
+++ sofie_midi_controller/sofie_midi_controller.ino
@@ -2,27 +2,29 @@
 #include <Bounce2.h>
 #include "./display.h"
 
-
 const int BTN_THRESHOLD = 25;
 const int BTN_1_PIN = 2;
 int i = 0;
+
 int VOL = 0;
 int BOOST = 0;
-
 Bounce debouncer = Bounce();
 
 void setup() {
   Serial.begin(31250);
-
   pinMode(BTN_1_PIN, INPUT_PULLUP);
   debouncer.attach(BTN_1_PIN); 
   debouncer.interval(BTN_THRESHOLD);
-
   lcdInit();
+  menuInit();
+  
+  renderMenu();
 }
+
 void loop() {
   debouncer.update();
   if (debouncer.fell()) {
+    navigate(cursor, 1);
     renderMenu();
     //i++;
     //Serial.write(0xC0);// Program Change channel 1
