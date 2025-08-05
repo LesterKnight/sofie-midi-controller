807fd552810b5289434cdf8f46a9ab3c9f122f1e
Oliveira Miranda <fishermen18@gmail.com>
2025-08-05 13:05:19 -0300

    Refactored data structure to OOP to improve readability and encapsulate menu behavior

--- sofie_midi_controller/display.h
+++ sofie_midi_controller/display.h
@@ -1,102 +1,118 @@
+#pragma once
+#include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
 
-// LCD I2C config
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-int cursorLine = 0;
+class Menu {
+private:
+  Menu* cursor = nullptr;
+  int cursorLine = 0;
+  static LiquidCrystal_I2C lcd;
+  bool root = false;
 
-//---------------------MENU STRUCT--------------
-typedef struct MenuNode {
-    char name[15];
-    MenuNode* submenu[4];
-    int submenuCount = 0;
-    MenuNode* parent;
-} MenuNode;
-
-MenuNode* mainMenu = (MenuNode*) malloc(sizeof(MenuNode));
-MenuNode* cursor = NULL;
-
-void createNode(char name[15], MenuNode* parent){
-  MenuNode* item = (MenuNode*) malloc(sizeof(MenuNode));
-  strcpy(item->name, name);
-  item->parent = parent;
-  parent->submenu[parent->submenuCount++] = item;
-}
-
-bool nodeExists(MenuNode* node){//EXISTS, NOT NULL?
-    if (node == NULL || node->parent == NULL) return false;
-    return true;
-}
-
-int calculateNodePos(MenuNode* node){//Calcula posicao do nó usando o pai
-  if(!nodeExists(node)) return -1; //EXISTS, NOT NULL?
-
-  MenuNode* parent = node->parent; //PARENT EXISTS!
-
-  for(int i=0;i< parent->submenuCount;i++){
-    if(parent->submenu[i] == node)
-      return i; //RETURN NODE POS
+public:
+  static const int submenuLen = 4;
+  char name[15];
+  Menu* submenu[submenuLen];
+  int submenuCount = 0;
+  Menu* parent;
+/*
+  static void log(const char* message) { //   Menu::log("Iniciado!");
+    Serial.println(message);
   }
-  return -1;
-}
+*/
+  Menu(const char* label = "MainMenu", Menu* parentNode = nullptr, bool isRoot = true) {
+    
+    strncpy(name, label, sizeof(name) - 1);
+    name[sizeof(name) - 1] = '\0';
+    parent = parentNode;
+    root = isRoot;
+    for (int i = 0; i < submenuLen; i++) submenu[i] = nullptr;
+  }
 
-bool validMove(MenuNode* node, int move, int nodepos){ //NEXT NODE EXISTS?
-  if(!nodeExists(node)) return false;//já verifica nulidade de parent
-  if(move == 1)
-     return ((nodepos+1) <= node->parent->submenuCount);
-  else if (move == 0)
-     return ((nodepos-1) >=0);
-  else return false;
-}
+  int length() {
+    int count = 0;
+    for (int i = 0; i < submenuLen; i++) {
+      if (submenu[i] != nullptr) count++;
+      else break;
+    }
+    return count;
+  }
 
-void navigate(MenuNode* node, int move) {
-  int nodepos = calculateNodePos(node);
-  Serial.println("pos atual:");
-  Serial.println(nodepos);
+  static bool nodeExists(Menu* node) {
+    return node != nullptr && node->parent != nullptr;
+  }
 
-  if(nodepos == -1) return;//NODE DOES NOT EXISTS
-  MenuNode* parent = node->parent;//PARENT EXISTS!
+  static int calculateNodePos(Menu* node) {
+    if (!nodeExists(node)) return -1;
+    Menu* parent = node->parent;
+    int count = parent->length();
+    for (int i = 0; i < count; i++) {
+      if (parent->submenu[i] == node) return i;
+    }
+    return -1;
+  }
 
+  bool validMove(int move, int nodePos) {
+    if (move == 1)
+      return (nodePos + 1) < parent->length()-1;
+    else if (move == 0)
+      return (nodePos - 1) >= 0;
+    else
+      return false;
+  }
 
+  void navigate(int move) {
+    int nodePos = calculateNodePos(cursor);
+    if (nodePos == -1) return;
 
-  if(move == 1 && validMove(node, move, nodepos+1)){//1 -> Next
-      if(cursorLine == 0){
+    Menu* parentNode = cursor->parent;
+    if (move == 1 && validMove(move, nodePos)) {
+      if (cursorLine == 0){
         cursorLine = 1;
+      } 
+      cursor = parentNode->submenu[nodePos + 1];
+      renderMenu();
+      Serial.println("move");
+    }
+  }
+
+  Menu* addChild(const char* label) {
+    if (submenuCount >= submenuLen) return nullptr;
+    Menu* child = new Menu( label, this, false);
+    submenu[submenuCount++] = child;
+    return child;
+  }
+
+  void init(){
+    lcd.init();
+    lcd.clear();
+    lcd.backlight();
+    cursor = addChild("Laranja");
+    addChild("Abacaxi");
+    addChild("VOLTAR");
+    renderMenu();
+  }
+
+  void renderMenu() {
+    lcd.clear();
+    int pos = calculateNodePos(cursor);
+    if (cursorLine == 0) {
+      lcd.setCursor(0, 0);
+      lcd.print(">");
+      lcd.print(cursor->name);
+      if (pos + 1 < cursor->parent->length()) {
+        lcd.setCursor(0, 1);
+        lcd.print(cursor->parent->submenu[pos + 1]->name);
       }
-      cursor = parent->submenu[nodepos+1];
-      
+    } else {
+      lcd.setCursor(0, 1);
+      lcd.print(">");
+      lcd.print(cursor->name);
+      if (pos - 1 >= 0) {
+        lcd.setCursor(0, 0);
+        lcd.print(cursor->parent->submenu[pos - 1]->name);
+      }
+    }
   }
-}
-
-void lcdInit(){
-  lcd.init();
-  lcd.backlight();
-}
-
-void renderMenu(){
-  lcd.clear();
-  if(cursorLine == 0){
-    lcd.setCursor(0, 0);
-    lcd.print(">");
-    lcd.print(cursor->name);
-    int pos = calculateNodePos(cursor);
-    lcd.setCursor(0, 1);
-    lcd.print(cursor->parent->submenu[pos+1]->name);
-  }
-  else{
-    lcd.setCursor(0, 1);
-    lcd.print(">");
-    lcd.print(cursor->name);
-    int pos = calculateNodePos(cursor);
-    lcd.setCursor(0, 0);
-    lcd.print(cursor->parent->submenu[pos-1]->name);
-  }
-}
-
-//-------------CREATE MENU
-void menuInit(){
-  createNode("Laranja", mainMenu);
-  createNode("Abacaxi", mainMenu);
-  createNode("Framboesa", mainMenu);
-  createNode("VOLTAR", mainMenu);
-  cursor = mainMenu->submenu[0];
-}
+};
+LiquidCrystal_I2C Menu::lcd(0x27, 16, 2);
--- sofie_midi_controller/sofie_midi_controller.ino
+++ sofie_midi_controller/sofie_midi_controller.ino
@@ -4,31 +4,21 @@
 
 const int BTN_THRESHOLD = 25;
 const int BTN_1_PIN = 2;
-int i = 0;
-
-int VOL = 0;
-int BOOST = 0;
 Bounce debouncer = Bounce();
 
+Menu menu;
 void setup() {
   Serial.begin(31250);
   pinMode(BTN_1_PIN, INPUT_PULLUP);
   debouncer.attach(BTN_1_PIN); 
   debouncer.interval(BTN_THRESHOLD);
-  lcdInit();
-  menuInit();
-  
-  renderMenu();
+  menu.init();
 }
 
 void loop() {
   debouncer.update();
   if (debouncer.fell()) {
-    navigate(cursor, 1);
-    renderMenu();
-    //i++;
-    //Serial.write(0xC0);// Program Change channel 1
-    //delay(50);
-    //Serial.write(i);
+    Serial.println("press");
+    menu.navigate(1);
   }
 }
