839508a343942e04e985f20cecdfba5b209e6b87
Oliveira Miranda <fishermen18@gmail.com>
2025-08-06 11:33:21 -0300

    Menu types and ENUM defined

--- sofie_midi_controller/display.h
+++ sofie_midi_controller/display.h
@@ -2,41 +2,27 @@
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
 
-enum MenuActions {
-  NEXT,
-  PREVIOUS,
-  ENTER,
-  BACK
-};
+  enum MenuActions {
+    NEXT,
+    PREVIOUS,
+    ENTER,
+  };
+
+  enum MenuType {
+    ROOT,
+    SUBMENU,
+    FUNCTION,
+    BACK
+  };
 
 class Menu {
 private:
   Menu* cursor = nullptr;
   int cursorLine = 0;
   static LiquidCrystal_I2C lcd;
-  bool root = false;
+  MenuType type;
 
-public:
-  static const int submenuLen = 7;
-  char name[15];
-  Menu* submenu[submenuLen];
-  int submenuCount = 0;
-  Menu* parent;
-/*
-  static void log(const char* message) { //   Menu::log("Iniciado!");
-    Serial.println(message);
-  }
-*/
-  Menu(const char* label = "MainMenu", Menu* parentNode = nullptr, bool isRoot = true) {
-    
-    strncpy(name, label, sizeof(name) - 1);
-    name[sizeof(name) - 1] = '\0';
-    parent = parentNode;
-    root = isRoot;
-    for (int i = 0; i < submenuLen; i++) submenu[i] = nullptr;
-  }
-
-  int length() {
+  int subMenulength() {
     int count = 0;
     for (int i = 0; i < submenuLen; i++) {
       if (submenu[i] != nullptr) count++;
@@ -52,48 +38,66 @@ public:
   static int calculateNodePos(Menu* node) {
     if (!nodeExists(node)) return -1;
     Menu* parent = node->parent;
-    int count = parent->length();
+    int count = parent->subMenulength();
     for (int i = 0; i < count; i++) {
       if (parent->submenu[i] == node) return i;
     }
     return -1;
   }
 
-bool validMove(MenuActions move, int nodePos) {
-  if (move == NEXT)
-    return (nodePos + 1) < parent->length()-1;
-  else if (move == PREVIOUS)
-    return (nodePos - 1) >= 0;
-  else
-    return false;
-}
-
-void navigate(MenuActions move) {
-  int nodePos = calculateNodePos(cursor);
-  if (nodePos == -1) return;
-
-  Menu* parentNode = cursor->parent;
-
-  if (move == NEXT && validMove(move, nodePos)) {
-    if (cursorLine == 0) cursorLine = 1;
-    cursor = parentNode->submenu[nodePos + 1];
-    renderMenu();
-    Serial.println("move");
-  } 
-  else if (move == PREVIOUS && validMove(move, nodePos)) {
-    Serial.println("move back start");
-    if (cursorLine == 1) cursorLine = 0;
-    cursor = parentNode->submenu[nodePos - 1];
-    renderMenu();
-    Serial.println("move back");
+  bool validMove(MenuActions move, int nodePos) {
+    if (move == NEXT)
+      return (nodePos + 1) < parent->subMenulength()-1;
+    else if (move == PREVIOUS)
+      return (nodePos - 1) >= 0;
+    else if (move == ENTER &&
+    ( 
+      cursor->type == SUBMENU||
+      cursor->type == FUNCTION||
+      cursor->type == BACK
+    )) return true;
+    else
+      return false;
   }
-}
 
-  Menu* addChild(const char* label) {
-    if (submenuCount >= submenuLen) return nullptr;
-    Menu* child = new Menu( label, this, false);
-    submenu[submenuCount++] = child;
-    return child;
+  void renderMenu() {
+    lcd.clear();
+    int pos = calculateNodePos(cursor);
+    if (cursorLine == 0) {
+      lcd.setCursor(0, 0);
+      lcd.print(">");
+      lcd.print(cursor->name);
+      if (pos + 1 < cursor->parent->subMenulength()) {
+        lcd.setCursor(0, 1);
+        lcd.print(cursor->parent->submenu[pos + 1]->name);
+      }
+    }
+    else if (cursorLine == 1){
+      lcd.setCursor(0, 0);
+      lcd.print(cursor->parent->submenu[pos - 1]->name);
+      lcd.setCursor(0, 1);
+      lcd.print(">");
+      lcd.print(cursor->name);
+    }
+  }
+
+public:
+  static const int submenuLen = 7;
+  char name[15];
+  Menu* submenu[submenuLen];
+  int submenuCount = 0;
+  Menu* parent;
+
+  Menu(const char* label = "MainMenu", Menu* parentNode = nullptr) {
+    strncpy(name, label, sizeof(name) - 1);
+    name[sizeof(name) - 1] = '\0';
+    parent = parentNode;
+    for (int i = 0; i < submenuLen; i++) submenu[i] = nullptr;
+
+    if(parentNode == nullptr)
+      type = ROOT;
+    else
+      type = SUBMENU;
   }
 
   void init(){
@@ -108,29 +112,40 @@ void navigate(MenuActions move) {
     addChild("VOLTAR");
     renderMenu();
   }
+  void navigate(MenuActions move) {
+    int nodePos = calculateNodePos(cursor);//remover validmove da loogica, rendermenu para o fim
+    if (nodePos == -1 || !validMove(move, nodePos)) return;
+    Menu* parentNode = cursor->parent;
 
-  void renderMenu() {
-    lcd.clear();
-    int pos = calculateNodePos(cursor);
 
+switch (move) {
+  case ENTER:
+    // cursor->subMenulength()>0
+    // cursor = cursor->parent;
+    break;
 
-    if (cursorLine == 0) {
-      lcd.setCursor(0, 0);
-      lcd.print(">");
-      lcd.print(cursor->name);
-      if (pos + 1 < cursor->parent->length()) {
-        lcd.setCursor(0, 1);
-        lcd.print(cursor->parent->submenu[pos + 1]->name);
-      }
-    }
-    else if (cursorLine == 1){
-      lcd.setCursor(0, 0);
-      lcd.print(cursor->parent->submenu[pos - 1]->name);
-      lcd.setCursor(0, 1);
-      lcd.print(">");
-      lcd.print(cursor->name);
-    }
+  case NEXT:
+    if (cursorLine == 0) cursorLine = 1;
+    cursor = parentNode->submenu[nodePos + 1];
+    break;
+
+  case PREVIOUS:
+    if (cursorLine == 1) cursorLine = 0;
+    cursor = parentNode->submenu[nodePos - 1];
+    break;
+
+  default:
+    return false;
+}
+    renderMenu();
+    delay(100);
   }
-  
+
+  Menu* addChild(const char* label) {
+    if (submenuCount >= submenuLen) return nullptr;
+    Menu* child = new Menu( label, this);
+    submenu[submenuCount++] = child;
+    return child;
+  }
 };
 LiquidCrystal_I2C Menu::lcd(0x27, 16, 2);
